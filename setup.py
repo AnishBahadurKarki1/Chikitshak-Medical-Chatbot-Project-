df39a758d7fd41d3ce9e4c0b935c657c3651eb67
AnishBahadurKarki <np03cs4a220011@heraldcollege.edu.np>
2026-02-05 19:55:08 +0545

    Folder Structure is Added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="chikitshak",
+    version="0.0.0",
+    author="Anish Bahadur Karki",
+    author_email="karkianish746@gmail.com",
+    packages=find_packages(),
+    
+)
+    
